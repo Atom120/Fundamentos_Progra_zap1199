0608c45500198e65e17e0306dc5a48b4b69e779d
j0emike <Clevereratom120@gmail.com>
2024-10-11 08:12:14 -0600

    atom120
    arregle lo de peleas

--- P007 Decisiones (IF-ELSE-SWITCH CASE)/P007 Decisiones (IF-ELSE-SWITCH CASE)/P007 Decisiones (IF-ELSE-SWITCH CASE).cpp
+++ P007 Decisiones (IF-ELSE-SWITCH CASE)/P007 Decisiones (IF-ELSE-SWITCH CASE)/P007 Decisiones (IF-ELSE-SWITCH CASE).cpp
@@ -11,10 +11,11 @@ int main()
     setlocale(LC_ALL, "en_MX.UTF-8");
     
 
+
     std::cout << "Hola estas en una arena de combate, primero que nada ¿cuántos luchadores ves?\n";
     std::cin >> peleadores;
     std::cout << "Muy bien, que tipo de pelea quieres" << std::endl;
-    std::cout << "1.- Eliminatoria directa 2.- Royal Rumble 3.- Grupos/semis y final" << std::endl;
+    std::cout << "1.- Royal Rumble 2.-Eliminatoria directa  3.- Grupos/semis y final" << std::endl;
     std::cin >> opcion;
     
     switch (opcion)
@@ -40,29 +41,40 @@ int main()
 
      
      break;
+     
 
     case 3:
-    
+        
         //Grupos/semis y final
         int grupo;
         int peleas;
         int semi;
-        
+        int fina;
 
-        grupo = peleadores / 4;//si son grupos de 4
-        peleas = grupo * 6;
-        std::cout << "En la fase de grupos hay " << peleas << " peleas" << std::endl;
-        std::cout << "Y pasan " << peleadores / 2 << " peleadores" << std::endl;
+            grupo = peleadores / 4;//si son grupos de 4
+            peleas = grupo * 6;
+            std::cout << "En la fase de grupos hay " << peleas << " peleas" << std::endl;
+            std::cout << "Y pasan " << peleadores / 2 << " peleadores" << std::endl;
 
-        //semifinal
-        semi = peleadores / 4;
-            std::cout << "En semis habría " << semi << " peleas" << std::endl;
+            if (peleadores >= 8)
+            {
+                //semifinal
+                semi = peleadores / 4;
+                std::cout << "En semis habría " << semi << " peleas" << std::endl;
 
-        //final
-            std::cout << "Para la final habría " << semi-1 << " peleas";
-    
-    break;
-    }
+                //final
+                fina = semi - 1;
+                std::cout << "Para la final habría " << fina << " peleas" << std::endl;
+                std::cout << "El total de peleas sera " << semi + peleas + fina;
+            }
+            else
+            {
+                std::cout << "En la final habría " << peleadores - 1 << " peleas " << std::endl;
+            }
+
+        break;
+     }
+     
 
     }
    
