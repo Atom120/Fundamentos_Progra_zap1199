19940d837448d04fce018774ee819f015d3d3781
sev_sgk <Clevereratom120@gmail.com>
2024-10-22 23:51:55 -0600

    sebas
    tarea 12

---
+++ P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR).cpp
@@ -0,0 +1,22 @@
+// P012 Bucles (WHILE-DO WHILE-FOR).cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include<locale.h>
+#include <string>
+#include<cstdlib>
+
+int main()
+{
+    bool com;
+    int vidas = 3;
+    std::cout << "Hola\n";
+    while (com)
+    {
+
+    }
+}
+/*
+Ciclo de juego infinito gastar 3 vidas, preguntar al usuario si quiere continuar
+Contar hasta 100
+*/
