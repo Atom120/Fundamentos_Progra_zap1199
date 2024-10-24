47c5d37f4090e4dd9453394e8ed46b24398f8722
sev_sgk <Clevereratom120@gmail.com>
2024-10-23 23:38:53 -0600

    sebas
    tarea 12

--- P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR).cpp
+++ P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR)/P012 Bucles (WHILE-DO WHILE-FOR).cpp
@@ -8,13 +8,31 @@
 
 int main()
 {
-    bool com;
+    int con;
     int vidas = 3;
-    std::cout << "Hola\n";
-    while (com)
-    {
 
+    std::cout << "Hola tienes 3 vidas, Para entrar al bucle presiona 0 y para salir cualquier otro numero entero cuando te pregunten\n";
+    std::cin >> con;
+
+    while (con == 0)
+    {
+                
+                for (int i = 3; i >= 0; i--)
+                {
+                    std::cout << "Dale al enter para perder vidas " << std::endl << "Ahora tienes " << i << std::endl;
+                    system("pause");
+                }
+
+                std::cout << "Quieres salir?? \n";
+                std::cin >> con;
     }
+    std::cout << "Ahora vamor a contar hasta 100\n";
+    for (int i = 0; i <= 100; i++)
+    {
+        std::cout << std::endl << i << std::endl;
+       
+    }
+
 }
 /*
 Ciclo de juego infinito gastar 3 vidas, preguntar al usuario si quiere continuar
