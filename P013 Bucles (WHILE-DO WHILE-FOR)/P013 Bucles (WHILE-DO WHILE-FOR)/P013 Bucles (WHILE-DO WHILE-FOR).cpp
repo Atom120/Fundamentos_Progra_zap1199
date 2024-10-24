f82b34f56786603eef23e283649e8de0f47ec314
j0emike <Clevereratom120@gmail.com>
2024-10-24 08:57:01 -0600

    sebas
    tarea 13

---
+++ P013 Bucles (WHILE-DO WHILE-FOR)/P013 Bucles (WHILE-DO WHILE-FOR)/P013 Bucles (WHILE-DO WHILE-FOR).cpp
@@ -0,0 +1,31 @@
+// P013 Bucles (WHILE-DO WHILE-FOR).cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <limits>
+#include <string>
+
+int main()
+{
+    std::string ato;
+    std::string ususario = "atom";
+    std::cout << "Hola ingresa tu usuario!\n";
+    std::cin >> ato;
+    do
+    {
+        if (ato == ususario)
+        {
+            std::cout << "Ya entratse \n";
+        }
+        else
+        {
+            std::cout << "Por favor ingresa el ususario coreccto\n";
+            std::cin >> ato;
+        }
+    } while (ato != ususario);
+}
+
+/*
+El login en un videojuego se repite si el usuario no ingresó los datos correctos.
+Si ingreso los datos correctos volver a pedir los datos.
+*/
