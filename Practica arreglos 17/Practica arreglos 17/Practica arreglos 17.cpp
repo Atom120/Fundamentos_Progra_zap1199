38a7b2dd4c88473c59611a9e3fb83b47f5f35ff8
sev_sgk <Clevereratom120@gmail.com>
2024-11-03 21:07:30 -0600

    sebas
    p0016 arreglos finitos

--- Practica arreglos 17/Practica arreglos 17/Practica arreglos 17.cpp
+++ Practica arreglos 17/Practica arreglos 17/Practica arreglos 17.cpp
@@ -1,21 +1,45 @@
 // Practica arreglos 17.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-
-int main()
-{
-    int num = 0;
+/*
+   int num = 0;
 
     int edades[3][2] = { {1,2},{9,8},{14,21} };
     int filas = (sizeof(edades) / sizeof(edades[1]));
     std::cout << "edades"<< edades << " edades[0] " << edades[0] << " " << filas << std::endl;
     int columnas = (sizeof(edades[1]) / sizeof(edades[1][1]));
     std::cout << columnas;
-    
-    int num[3][3];
-}
+*/
 
+#include <iostream>
+#include <string>
+#include<locale.h>
+
+int main()
+{
+
+    std::string nombre[3] = { "r6","fornite","metro" };
+    int ano[3] = {2015,2017,2019};
+    std::string Autor[3] = { "Andrea Zanini","Tim Sweeney","Dmitry Glukhovsky" };
+    std::string Estudio[3] = { "Ubisoft Montreal","Epic","4A Games" };
+
+    std::cout << nombre[0] << std::endl;
+    std::cout << ano[0] << std::endl;
+    std::cout << Autor[0] << std::endl;
+    std::cout << Estudio[0] << std::endl;
+    system("pause");
+
+    std::cout << nombre[1] << std::endl;
+    std::cout << ano[1] << std::endl;
+    std::cout << Autor[1] << std::endl;
+    std::cout << Estudio[1] << std::endl;
+    system("pause");
+
+    std::cout << nombre[2] << std::endl;
+    std::cout << ano[2] << std::endl;
+    std::cout << Autor[2] << std::endl;
+    std::cout << Estudio[2] << std::endl;
+    system("pause");
+}
+//Año de publicación, Autor, Estudio que lo creo
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
 
